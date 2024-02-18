06ab5ffc16416d112bfd6e49d3d5a4d951b0b379
Luana <luana@luana.dev.br>
2024-02-18 12:31:52 -0300

    Update Player.gd

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -29,8 +29,12 @@ var direction = Vector3.ZERO
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(-deg_to_rad(event.relative.x * mouse_sens))
-		head.rotate_x(-deg_to_rad(event.relative.y * mouse_sens))
-		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(90))
+		if not PlayerVariables.underwater:
+			head.rotate_x(-deg_to_rad(event.relative.y * mouse_sens))
+			head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(90))
+		else:
+			rotate_z(-deg_to_rad(event.relative.y * mouse_sens))
+			rotation.z = clamp(rotation.z, deg_to_rad(-89), deg_to_rad(90))
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -55,43 +59,42 @@ func _physics_process(delta):
 		
 	# We create a local variable to store the input direction.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	#direction = lerp(direction, ((transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()), delta*lerp_speed) - kinda buggy
-	direction = ((transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized())
+	direction = lerp(direction, ((transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()), delta*lerp_speed) #- kinda buggy
+	#direction = ((transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized())
 	# We check for each move input and update the direction accordingly.
 	if direction:
-		velocity.x = direction.x * current_speed
-		velocity.z = direction.z * current_speed
+		target_velocity.x = direction.x * current_speed
+		target_velocity.z = direction.z * current_speed
+		if PlayerVariables.underwater:
+			target_velocity.y = direction.y * current_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, current_speed)
-		velocity.z = move_toward(velocity.z, 0, current_speed)	
-	#if direction != Vector3.ZERO:
-	#	direction = direction.normalized()
-	#	$Pivot.look_at(position + direction, Vector3.UP)
-	# Ground Velocity
-	target_velocity.x = direction.x * current_speed
-	target_velocity.z = direction.z * current_speed
-	# Vertical Velocity
+		target_velocity.x = move_toward(velocity.x, 0, current_speed)
+		target_velocity.z = move_toward(velocity.z, 0, current_speed)	
+
 	if (not is_on_floor()) and (not PlayerVariables.underwater): # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y -= (gravity * delta)
-	# Jump
-	if is_on_floor() and Input.is_action_pressed("move_up"): 
-		target_velocity.y = jump_impulse
 	
-	if PlayerVariables.underwater and Input.is_action_pressed("move_up"): # todo: not move up forever, not keep moving down if fall in water
-		position.y += 1
+	if is_on_floor() and Input.is_action_pressed("move_up"): # Jump	
+		target_velocity.y = jump_impulse
+
+	# water movement up-down
+	if PlayerVariables.underwater and Input.is_action_pressed("move_up"):
+		target_velocity.y = current_speed
+	if PlayerVariables.underwater and Input.is_action_pressed("move_down"):
+		target_velocity.y = - current_speed
+		
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
 	
 	# Is she underwater?
 	if position.y <= 0:
-		PlayerVariables.underwater = not ("AirArea" in str(parea.get_overlapping_areas()))
+		PlayerVariables.underwater =  not ("AirArea" in str(parea.get_overlapping_areas()))
 	else:
-		PlayerVariables.underwater = ("WaterArea" in str(parea.get_overlapping_areas()))
-		
+		PlayerVariables.underwater =  ("WaterArea" in str(parea.get_overlapping_areas()))
 
 	# testing
 	print('underwater ' + str(PlayerVariables.underwater))
 	print(str(parea.get_overlapping_areas())) # This detects the areas!!
 	print(current_speed)
-
+	
