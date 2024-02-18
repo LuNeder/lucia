377b0d8d786b2467cc54845abe853be8745641c9
Luana <luana@luana.dev.br>
2024-02-18 13:33:40 -0300

    pqp pra rodar a camera direito eu vou ter q lembrar das aulas de algebra linear e geometria analitica ne

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -6,6 +6,7 @@ extends CharacterBody3D
 @onready var ray_cast_3d = $RayCast3D
 
 var sprinting = false
+var previous_uw = true
 
 # Speeds in meters per second.
 @export var current_speed = 7
@@ -25,16 +26,16 @@ var direction = Vector3.ZERO
 @export var lerp_speed = 10 
 
 
-
 func _input(event):
 	if event is InputEventMouseMotion:
+		print(event)
 		rotate_y(-deg_to_rad(event.relative.x * mouse_sens))
 		if not PlayerVariables.underwater:
 			head.rotate_x(-deg_to_rad(event.relative.y * mouse_sens))
 			head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(90))
 		else:
-			rotate_z(-deg_to_rad(event.relative.y * mouse_sens))
-			rotation.z = clamp(rotation.z, deg_to_rad(-89), deg_to_rad(90))
+			rotate_x(-deg_to_rad(event.relative.y * mouse_sens))
+			rotation.x = clamp(rotation.x, deg_to_rad(-89), deg_to_rad(90))
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -59,8 +60,11 @@ func _physics_process(delta):
 		
 	# We create a local variable to store the input direction.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	if PlayerVariables.underwater:
+		pass #input_dir = input_dir.rotated(Vector3(1, 0, 0), 0.5)
 	direction = lerp(direction, ((transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()), delta*lerp_speed) #- kinda buggy
 	#direction = ((transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized())
+
 	# We check for each move input and update the direction accordingly.
 	if direction:
 		target_velocity.x = direction.x * current_speed
@@ -74,7 +78,7 @@ func _physics_process(delta):
 	if (not is_on_floor()) and (not PlayerVariables.underwater): # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y -= (gravity * delta)
 	
-	if is_on_floor() and Input.is_action_pressed("move_up"): # Jump	
+	if is_on_floor() and Input.is_action_pressed("move_up"): # Jump
 		target_velocity.y = jump_impulse
 
 	# water movement up-down
@@ -87,14 +91,22 @@ func _physics_process(delta):
 	velocity = target_velocity
 	move_and_slide()
 	
+	previous_uw = PlayerVariables.underwater
 	# Is she underwater?
 	if position.y <= 0:
 		PlayerVariables.underwater =  not ("AirArea" in str(parea.get_overlapping_areas()))
 	else:
 		PlayerVariables.underwater =  ("WaterArea" in str(parea.get_overlapping_areas()))
+		
+	if PlayerVariables.underwater and not previous_uw:
+		rotation.x = (-deg_to_rad(90))
+	elif previous_uw and not PlayerVariables.underwater: 
+		rotation.x = 0
+		rotation.z = 0
 
 	# testing
 	print('underwater ' + str(PlayerVariables.underwater))
 	print(str(parea.get_overlapping_areas())) # This detects the areas!!
 	print(current_speed)
+	print(input_dir)
 	
