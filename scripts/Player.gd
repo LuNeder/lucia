d3c51fc706c9ca1f32597210e95edc296091076a
Luana <luana@luana.dev.br>
2024-02-17 22:20:35 -0300

    doing movement and 1person camera

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody3D
 @onready var head = $Head
+@onready var standing_collision_shape = $standing_CollisionShape3D
+@onready var crouching_collision_shape = $crouching_CollisionShape3D
+
 
 # How fast the player moves in meters per second.
 @export var current_speed = 7
@@ -10,8 +13,9 @@ extends CharacterBody3D
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")*10
 @export var jump_impulse = 40
 @export var crouching_depth = -0.5
+@export var heigh = 1.64/2
 
-#var lerp_speed = 100 - kinda buggy
+var lerp_speed = 10
 @export var mouse_sens = 0.4
 
 var target_velocity = Vector3.ZERO
@@ -20,6 +24,8 @@ var direction = Vector3.ZERO
 @onready var parea = $Area3D 
 
 var sprinting = false
+
+var underwater = PlayerVariables.underwater
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -32,9 +38,11 @@ func _ready():
 
 func _physics_process(delta):
 	#Movement
-	if Input.is_action_pressed("move_down") and not PlayerVariables.underwater: # todo: water movement
+	if Input.is_action_pressed("move_down") and not underwater: # todo: water movement
 		current_speed = crouching_speed
+		head.position.y = lerp(head.position.y, heigh + crouching_depth, delta*lerp_speed)
 	else:
+		head.position.y = lerp(head.position.y, heigh, delta*lerp_speed)
 		if Input.is_action_pressed("sprint"):
 			sprinting = true
 			current_speed = sprint_speed
@@ -47,7 +55,7 @@ func _physics_process(delta):
 		
 	# We create a local variable to store the input direction.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	#direction = lerp(direction, ((transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()), delta*lerp_speed)
+	#direction = lerp(direction, ((transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()), delta*lerp_speed) - kinda buggy
 	direction = ((transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized())
 	# We check for each move input and update the direction accordingly.
 	if direction:
@@ -63,10 +71,10 @@ func _physics_process(delta):
 	target_velocity.x = direction.x * current_speed
 	target_velocity.z = direction.z * current_speed
 	# Vertical Velocity
-	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+	if (not is_on_floor()) and (not underwater): # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y -= (gravity * delta)
 	# Jump
-	if is_on_floor() and Input.is_action_pressed("move_up"):
+	if (is_on_floor() or underwater) and Input.is_action_pressed("move_up"):
 		target_velocity.y = jump_impulse
 	# Moving the Character
 	velocity = target_velocity
@@ -74,13 +82,13 @@ func _physics_process(delta):
 	
 	# Is she underwater?
 	if position.y <= 0:
-		PlayerVariables.underwater = not ("AirArea" in str(parea.get_overlapping_areas()))
+		underwater = not ("AirArea" in str(parea.get_overlapping_areas()))
 	else:
-		PlayerVariables.underwater = ("WaterArea" in str(parea.get_overlapping_areas()))
+		underwater = ("WaterArea" in str(parea.get_overlapping_areas()))
 		
 
 	# testing
-	print('underwater ' + str(PlayerVariables.underwater))
+	print('underwater ' + str(underwater))
 	print(str(parea.get_overlapping_areas())) # This detects the areas!!
 	print(current_speed)
 
