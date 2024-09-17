6e00fd7840e7c50ea8abd60c33f8256b0d4702ef
Luana <luana@luana.dev.br>
2024-09-17 17:58:53 -0300

    Change to Double Precision
    hTerrain collision broke

--- addons/anthonyec.camera_preview/preview.gd
+++ addons/anthonyec.camera_preview/preview.gd
@@ -24,10 +24,10 @@ enum InteractionState {
 	ANIMATE_INTO_PLACE,
 }
 
-const margin_3d: Vector2 = Vector2(20, 20)
-const margin_2d: Vector2 = Vector2(40, 30)
-const min_panel_width: float = 250
-const max_panel_width_ratio: float = 0.6
+const margin_3d: Vector2 = Vector2(10, 10)
+const margin_2d: Vector2 = Vector2(20, 15)
+const panel_margin: float = 2
+const min_panel_size: float = 250
 
 @onready var panel: Panel = %Panel
 @onready var placeholder: Panel = %Placeholder
@@ -39,17 +39,18 @@ const max_panel_width_ratio: float = 0.6
 @onready var resize_right_handle: Button = %ResizeRightHandle
 @onready var lock_button: Button = %LockButton
 @onready var gradient: TextureRect = %Gradient
+@onready var viewport_margin_container: MarginContainer = %ViewportMarginContainer
+@onready var overlay_margin_container: MarginContainer = %OverlayMarginContainer
+@onready var overlay_container: Control = %OverlayContainer
 
 var camera_type: CameraType = CameraType.CAMERA_3D
 var pinned_position: PinnedPosition = PinnedPosition.RIGHT
 var viewport_ratio: float = 1
-var screen_scale: float = 1
+var editor_scale: float = EditorInterface.get_editor_scale()
 var is_locked: bool
 var show_controls: bool
 var selected_camera_3d: Camera3D
 var selected_camera_2d: Camera2D
-var remote_transform_3d: RemoteTransform3D
-var remote_transform_2d: RemoteTransform2D
 
 var state: InteractionState = InteractionState.NONE
 var initial_mouse_position: Vector2
@@ -57,7 +58,8 @@ var initial_panel_size: Vector2
 var initial_panel_position: Vector2
 
 func _ready() -> void:
-	screen_scale = DisplayServer.screen_get_scale()
+	# Set initial width.
+	panel.size.x = min_panel_size * editor_scale
 	
 	# Setting texture to viewport in code instead of directly in the editor 
 	# because otherwise an error "Path to node is invalid: Panel/SubViewport"
@@ -66,45 +68,75 @@ func _ready() -> void:
 	# This is a known issue:
 	# https://github.com/godotengine/godot/issues/27790#issuecomment-499740220
 	sub_viewport_text_rect.texture = sub_viewport.get_texture()
+	
+	# From what I can tell there's something wrong with how an editor theme
+	# scales when used within a plugin. It seems to ignore the screen scale. 
+	# For instance, a 30x30px button will appear tiny on a retina display.
+	#
+	# Someone else had the issue with no luck:
+	# https://forum.godotengine.org/t/how-to-scale-plugin-controls-to-look-the-same-in-4k-as-1080p/36151
+	#
+	# And seems Dialogic also scales buttons manually:
+	# https://github.com/dialogic-godot/dialogic/blob/master/addons/dialogic/Editor/Common/sidebar.gd#L25C6-L38
+	#
+	# Maybe I don't know the correct way to do it, so for now the workaround is
+	# to set the correct size in code using screen scale.
+	var button_size = Vector2(30, 30) * editor_scale
+	var margin_size: float = panel_margin * editor_scale
+	
+	resize_left_handle.size = button_size
+	resize_left_handle.pivot_offset = Vector2(0, 0) * editor_scale
+	
+	resize_right_handle.size = button_size
+	resize_right_handle.pivot_offset = Vector2(30, 30) * editor_scale
+	
+	lock_button.size = button_size
+	lock_button.pivot_offset = Vector2(0, 30) * editor_scale
+	
+	viewport_margin_container.add_theme_constant_override("margin_left", margin_size)
+	viewport_margin_container.add_theme_constant_override("margin_top", margin_size)
+	viewport_margin_container.add_theme_constant_override("margin_right", margin_size)
+	viewport_margin_container.add_theme_constant_override("margin_bottom", margin_size)
+	
+	overlay_margin_container.add_theme_constant_override("margin_left", margin_size)
+	overlay_margin_container.add_theme_constant_override("margin_top", margin_size)
+	overlay_margin_container.add_theme_constant_override("margin_right", margin_size)
+	overlay_margin_container.add_theme_constant_override("margin_bottom", margin_size)
+	
+	# Parent node overlay size is not available on first ready, need to wait a 
+	# frame for it to be drawn.
+	await get_tree().process_frame
+	
+	# Anchors are set in code because setting them in the editor UI doesn't take
+	# editor scale into account.
+	resize_left_handle.position = Vector2(0, 0)
+	resize_right_handle.set_anchors_preset(Control.PRESET_TOP_LEFT)
+	
+	resize_right_handle.position = Vector2(overlay_container.size.x - button_size.x, 0)
+	resize_right_handle.set_anchors_preset(Control.PRESET_TOP_RIGHT)
+	
+	lock_button.position = Vector2(0, overlay_container.size.y - button_size.y)
+	lock_button.set_anchors_preset(Control.PRESET_BOTTOM_LEFT)
 
 func _process(_delta: float) -> void:
 	if not visible: return
 	
-	var window_width = float(ProjectSettings.get_setting("display/window/size/viewport_width"))
-	var window_height = float(ProjectSettings.get_setting("display/window/size/viewport_height"))
-	viewport_ratio = window_height / window_width
-	
 	match state:
 		InteractionState.NONE:
-			# Constrain panel size to aspect ratio.
-			panel.size.y = panel.size.x * viewport_ratio
-			
-			# Clamp size.
-			panel.size = panel.size.clamp(
-				Vector2(min_panel_width * screen_scale, min_panel_width * screen_scale * viewport_ratio),
-				Vector2(size.x * max_panel_width_ratio, size.x * max_panel_width_ratio * viewport_ratio)
-			)
-			
+			panel.size = get_clamped_size(panel.size)
 			panel.position = get_pinned_position(pinned_position)
 			
 		InteractionState.RESIZE:
 			var delta_mouse_position = initial_mouse_position - get_global_mouse_position()
+			var resized_size = panel.size
 		
 			if pinned_position == PinnedPosition.LEFT:
-				panel.size = initial_panel_size - delta_mouse_position
+				resized_size = initial_panel_size - delta_mouse_position
 				
 			if pinned_position == PinnedPosition.RIGHT:
-				panel.size = initial_panel_size + delta_mouse_position
-				
-			# Constrain panel size to aspect ratio.
-			panel.size.y = panel.size.x * viewport_ratio
+				resized_size = initial_panel_size + delta_mouse_position
 			
-			# Clamp size.
-			panel.size = panel.size.clamp(
-				Vector2(min_panel_width * screen_scale, min_panel_width * screen_scale * viewport_ratio),
-				Vector2(size.x * max_panel_width_ratio, size.x * max_panel_width_ratio * viewport_ratio)
-			)
-			
+			panel.size = get_clamped_size(resized_size)
 			panel.position = get_pinned_position(pinned_position)
 			
 		InteractionState.DRAG:
@@ -156,9 +188,15 @@ func _process(_delta: float) -> void:
 	
 	# Sync camera settings.
 	if camera_type == CameraType.CAMERA_3D and selected_camera_3d:
-		# TODO: Don't think this is needed and can just assign `panel.size` directly.
-		var viewport_size = Vector2(panel.size.x, panel.size.x * viewport_ratio)
-		sub_viewport.size = viewport_size
+		sub_viewport.size = panel.size
+		
+		# Sync position and rotation without using a `RemoteTransform` node 
+		# because if you save a camera as a scene, the remote transform node will
+		# be stored within the scene. Also it's harder to keep the remote 
+		# transform `remote_path` up-to-date with scene changes, which causes 
+		# many errors.
+		preview_camera_3d.global_position = selected_camera_3d.global_position
+		preview_camera_3d.global_rotation = selected_camera_3d.global_rotation
 		
 		preview_camera_3d.fov = selected_camera_3d.fov
 		preview_camera_3d.projection = selected_camera_3d.projection
@@ -173,7 +211,8 @@ func _process(_delta: float) -> void:
 		preview_camera_3d.environment = selected_camera_3d.environment
 	
 	if camera_type == CameraType.CAMERA_2D and selected_camera_2d:
-		var ratio = window_width / panel.size.x
+		var project_window_size = get_project_window_size()
+		var ratio = project_window_size.x / panel.size.x
 		
 		# TODO: Is there a better way to fix this?
 		# The camera border is visible sometimes due to pixel rounding. 
@@ -183,6 +222,9 @@ func _process(_delta: float) -> void:
 		sub_viewport.size = panel.size
 		sub_viewport.size_2d_override = (panel.size - hide_camera_border_fix) * ratio
 		sub_viewport.size_2d_override_stretch = true
+		
+		preview_camera_2d.global_position = selected_camera_2d.global_position
+		preview_camera_2d.global_rotation = selected_camera_2d.global_rotation
 
 		preview_camera_2d.offset = selected_camera_2d.offset
 		preview_camera_2d.zoom = selected_camera_2d.zoom
@@ -212,15 +254,8 @@ func link_with_camera_3d(camera_3d: Camera3D) -> void:
 		
 	sub_viewport.disable_3d = false
 	sub_viewport.world_3d = camera_3d.get_world_3d()
-		
-	remote_transform_3d = RemoteTransform3D.new()
 	
-	remote_transform_3d.remote_path = preview_camera_3d.get_path()
-	remote_transform_3d.use_global_coordinates = true
-	
-	camera_3d.add_child(remote_transform_3d)
 	selected_camera_3d = camera_3d
-	
 	camera_type = CameraType.CAMERA_3D
 	
 func link_with_camera_2d(camera_2d: Camera2D) -> void:
@@ -240,23 +275,14 @@ func link_with_camera_2d(camera_2d: Camera2D) -> void:
 	sub_viewport.disable_3d = true
 	sub_viewport.world_2d = camera_2d.get_world_2d()
 		
-	remote_transform_2d = RemoteTransform2D.new()
-	
-	remote_transform_2d.remote_path = preview_camera_2d.get_path()
-	remote_transform_2d.use_global_coordinates = true
-	
-	camera_2d.add_child(remote_transform_2d)
 	selected_camera_2d = camera_2d
-	
 	camera_type = CameraType.CAMERA_2D
 
 func unlink_camera() -> void:
 	if selected_camera_3d:
-		selected_camera_3d.remove_child(remote_transform_3d)
 		selected_camera_3d = null
 	
 	if selected_camera_2d:
-		selected_camera_2d.remove_child(remote_transform_2d)
 		selected_camera_2d = null
 	
 	is_locked = false
@@ -270,10 +296,10 @@ func request_show() -> void:
 	visible = true
 	
 func get_pinned_position(pinned_position: PinnedPosition) -> Vector2:
-	var margin: Vector2 = margin_3d
+	var margin: Vector2 = margin_3d * editor_scale
 	
 	if camera_type == CameraType.CAMERA_2D:
-		margin = margin_2d
+		margin = margin_2d * editor_scale
 	
 	match pinned_position:
 		PinnedPosition.LEFT:
@@ -284,6 +310,73 @@ func get_pinned_position(pinned_position: PinnedPosition) -> Vector2:
 			assert(false, "Unknown pinned position %s" % str(pinned_position))
 			
 	return Vector2.ZERO
+	
+func get_clamped_size(desired_size: Vector2) -> Vector2:
+	var viewport_ratio = get_project_window_ratio()
+	var editor_viewport_size = get_editor_viewport_size()
+
+	var max_bounds = Vector2(
+		editor_viewport_size.x * 0.6,
+		editor_viewport_size.y * 0.8
+	)
+	
+	var clamped_size = desired_size
+	
+	# Apply aspect ratio.
+	clamped_size = Vector2(clamped_size.x, clamped_size.x * viewport_ratio)
+	
+	# Clamp the max size while respecting the aspect ratio.
+	if clamped_size.y >= max_bounds.y:
+		clamped_size.x = max_bounds.y / viewport_ratio
+		clamped_size.y = max_bounds.y
+		
+	if clamped_size.x >= max_bounds.x:
+		clamped_size.x = max_bounds.x
+		clamped_size.y = max_bounds.x * viewport_ratio
+	
+	# Clamp the min size based on if it's portrait or landscape. Portrait min
+	# size should be based on it's height. Landscape min size is based on it's
+	# width instead. Applying min width to a portrait size would make it too big.
+	var is_portrait = viewport_ratio > 1
+	
+	if is_portrait and clamped_size.y <= min_panel_size * editor_scale:
+		clamped_size.x = min_panel_size / viewport_ratio
+		clamped_size.y = min_panel_size
+		clamped_size = clamped_size * editor_scale
+		
+	if not is_portrait and clamped_size.x <= min_panel_size * editor_scale:
+		clamped_size.x = min_panel_size
+		clamped_size.y = min_panel_size * viewport_ratio
+		clamped_size = clamped_size * editor_scale
+	
+	# Round down to avoid sub-pixel artifacts, mainly seen around the margins.
+	return clamped_size.floor()
+	
+func get_project_window_size() -> Vector2:
+	var window_width = float(ProjectSettings.get_setting("display/window/size/viewport_width"))
+	var window_height = float(ProjectSettings.get_setting("display/window/size/viewport_height"))
+	
+	return Vector2(window_width, window_height)
+	
+func get_project_window_ratio() -> float:
+	var project_window_size = get_project_window_size()
+	
+	return project_window_size.y / project_window_size.x
+	
+func get_editor_viewport_size() -> Vector2:
+	var fallback_size = EditorInterface.get_editor_main_screen().size
+	
+	# There isn't an API for getting the viewport node. Instead it has to be
+	# found by checking the parent's parent of the subviewport and find
+	# the correct node based on name and class.
+	var editor_sub_viewport_3d = EditorInterface.get_editor_viewport_3d(0)
+	var editor_viewport_container = editor_sub_viewport_3d.get_parent().get_parent().get_parent()
+	
+	# Early return incase editor tree structure has changed.
+	if editor_viewport_container.get_class() != "Node3DEditorViewportContainer":
+		return fallback_size
+		
+	return editor_viewport_container.size
 
 func _on_resize_handle_button_down() -> void:
 	if state != InteractionState.NONE: return
